4dc2cee75321b27e9453b50bb8b17b51dc88adbd
toshiki0518 <toshiking0518@gmail.com>
2022-06-09 09:51:21 +0900

    developing

---
+++ algorithm/src/exercise/search/hash.py
@@ -0,0 +1,38 @@
+from collections import deque
+import queue
+import sys
+import math
+import random
+import datetime
+import time
+
+    
+class Disctionary:
+    S = []
+    n = 0
+    T = []
+    page=dict()
+    q=0
+    MAX_N = 10
+    MAX_Q = 4
+
+    def __init__(self, n = 10, q = 5):
+        test
+        
+    def insert(self, str):
+        self.page.append()
+        
+def main():
+    print('start')
+    start_time = time.time()
+    print(start_time)
+    # test()
+    binary = Binary(5, 3)
+    binary.exec()
+    end_time = time.time()
+    print(end_time - start_time)
+    print('end')
+        
+    
+if __name__ == '__main__':
+    main()
