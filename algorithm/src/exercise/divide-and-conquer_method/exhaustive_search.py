a5cbf0a59e4c482d0de2c723d8edccc397cf46fd
toshiki0518 <toshiking0518@gmail.com>
2022-06-20 21:27:34 +0900

    develop complete exhaustive search

--- algorithm/src/exercise/divide-and-conquer_method/exhaustive_search.py
+++ algorithm/src/exercise/divide-and-conquer_method/exhaustive_search.py
@@ -7,36 +7,42 @@ import random
 import datetime
 import time
 import sys
+import string
 
 sys.setrecursionlimit(20)
 
 class Exhaustive_search:
     n=0
-    S=[]
+    A=[]
+
+    m=[]
+    q=0
         
+    S=[]
+    result=[]
     def __init__(self, test = None) -> None:
         if test is not None:
             # self.S = [1,2,3,4]
             self.n = 3
             return
-        self.n = int(input('input number n:'))
-        A = []
-        for i in range(n):
-            num = int(input('input number A:'))
-            A.append(num)
-        q = int(input('input number q:'))
-        m=[]
-        for i in range(n):
-            num = int(input('input number m:'))
-            m.append(num)
 
+    def init1(self):
+        self.n = int(input('input number n:'))
+        
+        for i in range(self.n):
+            num = int(input('input number A:'))
+            self.A.append(num)
+        self.q = int(input('input number q:'))
+        for i in range(self.q):
+            num = int(input('input number m:'))
+            self.m.append(num)
+        
     def sample(self):
         self.make_combination()
         
     def make_combination(self):
         n = self.n
         for i in range(n):
-            print('n:%d,i:%d' % (n,i))
             self.S.insert(i, 0) # i を選択しない
         self.rec(0)
         
@@ -49,19 +55,50 @@ class Exhaustive_search:
         self.S[i] = 1 # i を選択
         self.rec(i+1)
         self.S[i] = 0 # i を選択しない
-        
+    
+    def randomname(self, n):
+        randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+        return ''.join(randlst)
+    
+    def solve(self, i, m):
+        # str = self.randomname(i)
+        # print('i:%d,m:%d,n:%d'%(i,m,self.n))
+        if m==0:
+            return 1
+        if i>= self.n:
+            return 0
+        # print('res1_str:%s'% str)
+        res1=self.solve(i+1, m)
+        # print('res2_str:%s'% str)
+        # print('A[i]:%d'% self.A[i])
+        res2=self.solve(i+1, m - self.A[i])
+        # print('res1:%d,res2:%d'%(res1,res2))        
+        self.result.append([res1,res2])
+        print('i:%d,m:%d,n:%d'%(i,m,self.n))
+        print('res1:%d,res2:%d' % (res1,res2) )
+        return res1 or res2
+    
+    def check(self):
+        self.init1()
+        for num in self.m:
+            if self.solve(0, num):
+                print('yes')
+            else:
+                print('no')
+        # print(self.result)
         
 def main():
     exhaustive_search = Exhaustive_search(True)
     print('start')
     start_time = time.time()
     print(start_time)
-    exhaustive_search.sample()
+    # exhaustive_search.sample()
+    exhaustive_search.check()
     end_time = time.time()
     print(end_time)
     print(end_time - start_time)
     print('end')
-        
+    
     
 if __name__ == '__main__':
     main()
