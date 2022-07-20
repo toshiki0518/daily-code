c7ed4ea317876b3a30454ae8baa3acd3c5fe4f84
toshiki0518 <toshiking0518@gmail.com>
2022-07-20 09:26:37 +0900

    start data structure

---
+++ pygorithm/src/data-structure/excercise.py
@@ -0,0 +1,18 @@
+
+from unittest import result
+
+
+class DataStructure:
+    def __init__(self) -> None:
+        pass
+   
+    def stack(self):
+       pass
+   
+def main():
+    pass
+    
+    
+if __name__ =='__main__':
+    main()
+    
