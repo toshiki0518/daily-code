2cafe3e813507545befead01fc03170cda2312eb
toshiki0518 <toshiking0518@gmail.com>
2023-12-03 13:33:10 +0900

    Design an O(N) algorithm to find the maximum difference     between any two distinct integers among N different integers, a0 through an-1.

---
+++ py/src/design_technique/brute_force/test.py
@@ -0,0 +1,14 @@
+import unittest
+from chapter_exercises import exercise33  
+
+
+class TestExercise33(unittest.TestCase):
+    def test_max_difference(self):
+        # テスト対象の関数を呼び出し
+        exercise33()
+
+        # テスト対象の関数がエラーをスローしないことを確認
+        self.assertTrue(True)  # 何もしない場合の例
+
+if __name__ == '__main__':
+    unittest.main()
