e660edeb4ba157074a8eb8321d5971a5d1d59ba1
toshiki0518 <toshiking0518@gmail.com>
2023-12-04 22:33:11 +0900

    test

--- py/src/design_technique/brute_force_2.py
+++ py/src/design_technique/brute_force_2.py
@@ -3,6 +3,39 @@ import os
 import math
 import random
 
+import sys
+
+# コマンドライン引数を取得
+# # arguments = sys.argv[1:]
+
+# # def test_value():
+# #     # 引数がまだ残っている場合
+# #     if arguments:
+# #         try:
+# #             # 次の引数を取得して整数に変換
+# #             value = int(arguments.pop(0))
+# #             return value
+# #         except ValueError:
+# #             # 整数に変換できない場合はエラー処理
+# #             print("無効な引数です。整数を指定してください。")
+# #             return None
+# #     else:
+# #         # 引数がもう残っていない場合
+# #         print("引数が不足しています。")
+# #         return None
+
+# # if __name__ == "__main__":
+# # get_value 関数を呼び出して次の引数を取得
+# value1 = test_value()
+# if value1 is not None:
+#     print(f"取得した値1: {value1}")
+
+# # 再度 get_value 関数を呼び出して次の引数を取得
+# value2 = test_value()
+# if value2 is not None:
+#     print(f"取得した値2: {value2}")
+
+
 def cal_dist(x1: float, y1: float, x2: float, y2: float):
     print(x1,y1,x2,y2)
     x = (x1 - x2 )
@@ -36,28 +69,33 @@ def closest_point():
 class BruteForce:
     INF = 200000000
     def __init__(self) -> None:
+        self.arguments = self.get_arguments()
         print("class:", self.__class__.__name__)
         self.random_integers = []
         self.random_integers = self.get_random_num_list()
 
-    def get_random_num_list(self):
-        random_integers = [random.randint(1, 10) for _ in range(5)]
+
+    def get_arguments(self):
+        print(sys.argv[1:])
+        return sys.argv[1:]
+
+
+    def get_random_num_list(self, size = 5):
+        random_integers = [random.randint(1, 10) for _ in range(size)]
         print(random_integers)
         return random_integers
 
     def get_value(self):
-        if len(sys.argv) > 1:
+        if len(self.arguments) > 0:
             try:
-                param1 = int(sys.argv[1])
+                param1 = int(self.arguments.pop(0))
                 return param1
             except ValueError:
-                print('value error')
-                return 0
+                print('value num error')
+                return -1
         else:
-            print('value error')
-            return 0
-
-# 
+            print('value empty error')
+            return -1
 
 class LinerSearch(BruteForce):
     """_summary_
@@ -105,14 +143,23 @@ class LinerSearch(BruteForce):
                     print("Min value is %d." % (min_value))
         return min_value
 
-
+    def subset_sub_problem(self):
+        sum_list = []
+        target = self.get_value()
+        result = 0
+        for index, i in enumerate(self.get_random_num_list(self.get_value())):
+            print("index: %d" % index)
+    
 def main():
     ls = LinerSearch()
     # result = ls.search()
     # result = ls.found_id()
     # result = ls.get_min_value()
-    result = ls.search_pair()
-    print(result)
+    # result = ls.search_pair()
+    # result = ls.get_value()
+    # result = ls.get_value()
+    result = ls.subset_sub_problem()
+    # print(result)
 
 
 
